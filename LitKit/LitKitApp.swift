0a61cb4aefa5d5b1938395721a69ef1a648720e2
Taylor Fausak <taylor@fausak.me>
2026-02-24 21:37:41 -0600

    Initial Commit

---
+++ LitKit/LitKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitKitApp.swift
+//  LitKit
+//
+//  Created by Taylor Fausak on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct LitKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LitKitTests/LitKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  LitKitTests.swift
+//  LitKitTests
+//
+//  Created by Taylor Fausak on 2/24/26.
+//
+
+import Testing
+@testable import LitKit
+
+struct LitKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
