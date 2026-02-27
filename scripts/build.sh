201c879c79d575c53c0f84d39a319a5814524e75
Taylor Fausak <taylor@fausak.me>
2026-02-27 14:54:42 -0600

    Save script output to file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
+set -o xtrace
 exec xcodebuild \
   -scheme Pylo \
   -destination 'platform=iOS Simulator,name=iPhone 16e' \
   -quiet \
-  build "$@"
+  build "$@" 2>&1 | tee _scratch/build-$( date +%Y-%m-%d-%H-%M-%S ).txt
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
+set -o xtrace
 exec xcodebuild \
   -scheme Pylo \
   -destination 'platform=iOS Simulator,name=iPhone 16e' \
   -quiet \
-  test "$@"
+  test "$@" 2>&1 | tee _scratch/test-$( date +%Y-%m-%d-%H-%M-%S ).txt
